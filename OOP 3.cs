320ca8a66a5af6a9e4945017b7c8d61f027d893b
mcgalihanggara <mcgalihanggara@gmail.com>
2019-04-01 15:05:01 +0700

    Modified OOP 3.cs

--- OOP 3.cs
+++ OOP 3.cs
@@ -46,23 +46,64 @@ class Person {
 
     class Employee : Person
     {
+        private int nik;
 
+        public int NIK {
+            set
+            {
+                this.nik = value;
+            }
+            get
+            {
+                return this.nik;
+            }
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("I am working");
+        }
     }
 
     class Student : Person
     {
+        private int nis;
 
+        public int NIS
+        {
+            set
+            {
+                this.nis = value;
+            }
+            get
+            {
+                return this.nis;
+            }
+        }
+
+        public void Study()
+        {
+            Console.WriteLine("I am studying");
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Person student = new Person();
-            Student student2 = new Student();
-            student.Eat();
-            student.Sleep();
+            Employee employee = new Employee();
+            Person person;
 
+            employee.NIK = 2045;
+            employee.Name = "Jhon";
+            employee.Address = "LA";
+            employee.Age = 25;
+            Console.WriteLine("{0} {1} {2} {3}", employee.NIK, employee.Name, employee.Address, employee.Age);
+
+            person = employee;
+            Console.WriteLine("{0} {1} {2}", person.Name, person.Address, person.Age);
+            Employee empTmp = (Employee)person;
+            Console.WriteLine("{0} {1} {2} {3}", empTmp.NIK, empTmp.Name, empTmp.Address, empTmp.Age);
 
             Console.ReadKey();
         }
