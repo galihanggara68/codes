ad3d040ac4027ec1eacf1593e317911a2b828f5e
mcgalihanggara <mcgalihanggara@gmail.com>
2019-04-09 11:19:00 +0700

    Added ADO .Net 1.cs

--- ADO .Net 1.cs
+++ ADO .Net 1.cs
@@ -45,6 +45,20 @@
 
             connection.Close();
         }
+		
+		public void SelectEmployees(int employeeId) {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("select * from hr.copy_emp where employee_id = @empId", connection);
+            command.Parameters.AddWithValue("@empId", employeeId);
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine("First Name : {0} - Last Name : {1}", reader["first_name"], reader["last_name"]);
+            }
+
+            connection.Close();
+        }
     }
 	
 	class Program
