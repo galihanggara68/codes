2d157384e5a31d2bccca986015dae7fc5e7ccf68
mcgalihanggara <mcgalihanggara@gmail.com>
2019-04-11 15:27:19 +0700

    Added Post Data Folder

---
+++ ASP .Net/Post Data/Home.aspx.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPWebForm
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string nama = Request.Form["nama"];
+            if("Galih".Equals(nama))
+                Response.Redirect("~/Index.aspx");
+            Response.Write(nama);
+        }
+    }
+}
