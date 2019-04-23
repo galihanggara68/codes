c284514248873ea3521aafb521162a5365d3b9d7
Galih Anggara <mcgalihanggara@gmail.com>
2019-04-23 11:54:50 +0700

    Update IndexController

--- ASP .Net MVC/IndexController.cs
+++ ASP .Net MVC/IndexController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMVC.Models;
 
 namespace WebMVC.Controllers
 {
@@ -12,19 +13,24 @@ namespace WebMVC.Controllers
         [Route("")]
         [HttpGet]
         public ActionResult ListEmployee() {
-            if(TempData.Peek("names") == null) {
-                List<string> names = new List<string> { "Ucup", "Udin", "Ujang" };
-                TempData["names"] = names;
+            if(TempData.Peek("employees") == null) {
+                List<Employee> employees = new List<Employee> {
+                    new Employee{ EmployeeId = 100, FirstName = "Udin", LastName = "Jalaludin" },
+                    new Employee{ EmployeeId = 101, FirstName = "Ice", LastName = "Juice" },
+                    new Employee{ EmployeeId = 102, FirstName = "Ucup", LastName = "Aja" }
+                };
+
+                TempData["employees"] = employees;
             }
             return View("Index");
         }
 
         [Route("")]
         [HttpPost]
-        public ActionResult AddEmployee(string name)
+        public ActionResult AddEmployee(Employee employee)
         {
-            List<string> names = (List<string>)TempData.Peek("names");
-            names.Add(name);
+            List<Employee> employees = (List<Employee>)TempData.Peek("employees");
+            employees.Add(employee);
             return Redirect("~/employees");
         }
 
@@ -38,33 +44,34 @@ namespace WebMVC.Controllers
         [Route("{id}")]
         [HttpGet]
         public ActionResult GetEmployee(int id) {
-            List<string> names = (List<string>)TempData.Peek("names");
-            ViewBag.name = names[id];
+            List<Employee> employees = (List<Employee>)TempData.Peek("employees");
+            ViewBag.employee = employees.Find(e => e.EmployeeId == id);
             return View("Employee");
         }
 
         [Route("{id}/edit")]
         [HttpGet]
         public ActionResult EditEmployee(int id) {
-            List<string> names = (List<string>)TempData.Peek("names");
-            ViewBag.id = id;
-            ViewBag.name = names[id];
-            return View("EmployeeForm");
+            List<Employee> employees = (List<Employee>)TempData.Peek("employees");
+            ViewBag.employee = employees.Find(e => e.EmployeeId == id);
+            return View("EditForm");
         }
 
-        [Route("{id}/edit")]
+        [Route("edit")]
         [HttpPost]
-        public ActionResult UpdateEmployee(int id, string name) {
-            List<string> names = (List<string>)TempData.Peek("names");
-            names[id] = name;
+        public ActionResult UpdateEmployee(Employee edittedEmployee) {
+            List<Employee> employees = (List<Employee>)TempData.Peek("employees");
+            int index = employees.FindIndex(e => e.EmployeeId == edittedEmployee.EmployeeId);
+            employees[index] = edittedEmployee;
             return Redirect("~/employees");
         }
 
         [Route("{id}/delete")]
         [HttpPost]
         public ActionResult DeleteEmployee(int id) {
-            List<string> names = (List<string>)TempData.Peek("names");
-            names.RemoveAt(id);
+            List<Employee> employees = (List<Employee>)TempData.Peek("employees");
+            Employee employee = employees.Find(e => e.EmployeeId == id);
+            employees.Remove(employee);
             return Redirect("~/employees");
         }
     }
